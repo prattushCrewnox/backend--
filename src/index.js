80dec781376fc8f114d22ac600afdfd9593a348f
prattushCrewnox <prattushcrewnox@gmail.com>
2025-07-10 17:52:38 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import app from './app.js';
+import connectDB from './db/db.js';
+import dotenv from 'dotenv'
+
+connectDB()
+.then(() =>{
+    app.on('error', (error) => {
+        console.error('Error with: ', process.env.MONGODB_URI, error)
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log("Server is running at port:", process.env.PORT || 8000)
+    })
+})
+.catch(err => {
+    console.log("mongoDB connection Failed", err)
+})
---
+++ src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Errors {
+    constructor(statusCode,
+        message = "An error occurred",
+        stack = "",
+        errors = []) {
+        super(message);
+        this.statusCode = statusCode;
+        this.stack = stack;
+        this.errors = errors;
+        if (stack) {
+            this.stack = stack;
+        }
+        else {
+            Error.captureStackTrace(this, this.constructor);
+            this.stack = this.stack || new Error().stack;
+        }
+    }
+}
+export default ApiError;
